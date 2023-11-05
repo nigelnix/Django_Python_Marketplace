fa8008d8b1ce4368a31a5fc4686712820e47e3e2
nigelnix <nigel.nix@outlook.com>
2023-11-05 19:55:51 +0100

    update nov5 images added to items on front page

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'core/index.html')
+from item.models import Category, Item
 
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6] 
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
