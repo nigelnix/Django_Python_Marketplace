146dc38e6f07ee76e7150288ece04fc4fe37bec5
nigelnix <nigel.nix@outlook.com>
2023-11-08 21:22:37 +0100

    added signup page and login page nov6

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6] 
@@ -12,3 +14,20 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+       
+            
+    
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
